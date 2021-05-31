2e8688cf5dcb02c5475b259b30235d9c9f2107fc
bprab <83753524+bprab@users.noreply.github.com>
2021-05-31 17:09:16 +0900

    index

---
+++ index/index.js
@@ -0,0 +1,39 @@
+$(function() {
+    $(".sec01").vegas({
+        slides: [
+            { src: '/index/images/세종점베너.jpg', delay: 3500 },
+            { src: '/index/images/tararosa.png', delay: 3500 },
+            { src: '/index/images/테라로사제주_02.png', delay: 3500 }
+        ],
+        transition: 'fade',
+        animation: 'kenburns'
+    });
+    $(".sec02").vegas({
+        slides: [
+            { src: '/index/images/audi_main.png', delay: 3500 },
+            { src: '/index/images/audi.png', delay: 3500 },
+            { src: '/index/images/audisec2.png', delay: 3500 }
+        ],
+        transition: 'fade',
+        animation: 'kenburns'
+    });
+
+    var spot1 = $("header").offset().top;
+    var spot2 = $(".sec01").offset().top;
+    var spot3 = $("footer").offset().top;
+    $("#p1").click(function(){
+        $("html, body").animate({
+            scrollTop: spot1
+        },500);
+    });
+    $("#p2").click(function(){
+        $("html, body").animate({
+            scrollTop: spot2
+        },500);
+    });
+    $("#p3").click(function(){
+        $("html, body").animate({
+            scrollTop: spot3
+        },500);
+    });
+});
